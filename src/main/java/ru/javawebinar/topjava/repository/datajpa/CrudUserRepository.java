cde2bb712b3f14e34934f6af94321b6d3793367c
Elena Belyasnik <elena.belyasnik@yandex.ru>
2024-10-16 22:45:17 +0300

    HW06 fix dublicates error in test

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -1,12 +1,13 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.User;
+
+import javax.persistence.QueryHint;
+
+import static org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH;
 
 @Transactional(readOnly = true)
 public interface CrudUserRepository extends JpaRepository<User, Integer> {
@@ -15,6 +16,9 @@ public interface CrudUserRepository extends JpaRepository<User, Integer> {
     @Query("DELETE FROM User u WHERE u.id=:id")
     int delete(@Param("id") int id);
 
+    @QueryHints({
+            @QueryHint(name = HINT_PASS_DISTINCT_THROUGH, value = "false")
+    })
     User getByEmail(String email);
 
     @EntityGraph(attributePaths = {"meals", "roles"})
