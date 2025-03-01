1681710001ba8fcc82a769645e22a4866f30e887
Elena Belyasnik <elena.belyasnik@yandex.ru>
2025-03-01 18:35:46 +0300

    HW09 Binding/Update/Validation

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -1,7 +1,17 @@
 let form;
 
-function makeEditable(datatableApi) {
-    ctx.datatableApi = datatableApi;
+function makeEditable(datatable) {
+    ctx.datatableApi = $("#datatable").DataTable(
+        {
+            ...datatable,
+            "ajax": {
+                "url":ctx.ajaxUrl,
+                "dataSrc": ""
+            },
+            "paging": false,
+            "info": true
+        }
+    );
     form = $('#detailsForm');
 
     $(document).ajaxError(function (event, jqXHR, options, jsExc) {
